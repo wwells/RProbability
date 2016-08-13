3618335d90796867df14a6fce256ba6b8d1c738d
wwells <walt@occ-data.org>
2016-08-13 12:18:28 -0700

    feat(poisson)

--- poissDist.R
+++ poissDist.R
@@ -1,9 +1,20 @@
 #generate poisson dist graphs in R
 #In probability theory and statistics, 
-#the binomial distribution with parameters n and p 
-#is the discrete probability distribution of the number of 
-#successes in a sequence of n independent yes/no experiments, 
-#each of which yields success with probability p. 
+#the poisson dist is also for count data, and relies on 
+#x and lambda. 
+#as lambda grows large, Poisson begins to resemble the normal distribution
 
 # from R For Everyone
 library(ggplot2)
+
+pmf <- function(x, lambda) {
+    ((lambda^x)*exp(1)^(-lambda))/factorial(x)
+}
+
+#same as "dpois"
+
+# in poisson, lambda is both mean and variance
+
+
+p <- ggplot() + ggtitle(expression("Normal Distribution" == paste(frac(1, sqrt(2 * pi * sigma^2)),
+                                                          " ", e^{frac(-(x - mu)^2, 2 * sigma^2)})))
